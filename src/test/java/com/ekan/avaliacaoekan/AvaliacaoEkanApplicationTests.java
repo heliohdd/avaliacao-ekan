4e4385bc7101277b7ff4d4143dd34f64a281da78
Helio Dourado <heliohdd@yahoo.com.br>
2024-04-07 09:30:10 -0300

    Created project web with MySQL and JPA dependencies

---
+++ src/test/java/com/ekan/avaliacaoekan/AvaliacaoEkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekan.avaliacaoekan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoEkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
